db0d895edcf37f18239dd53f80b4d4219c1ffda8
Robert Boespflug <boespflr@oregonstate.edu>
2020-10-09 23:18:46 -0700

    refeactored email_blast to be easier ti read

--- email_blast/email_blast.js
+++ email_blast/email_blast.js
@@ -1,91 +1,129 @@
-//google
-const fs = require("fs")
-const { google } = require("googleapis")
-const readline = require("readline")
+/* *SETUP */
 
-//user
-const util = require("util")
-require("dotenv").config()
+
+//google sheets api and email 
+const { google } = require("googleapis")
 const nodemailer = require("nodemailer")
+
+//file i/o
+const fs = require("fs")
+const util = require("util")
+const readFileAsync = util.promisify(fs.readFile)
+
+//loads environment variables
+require("dotenv").config()
+
+//user made imported files
 const create_email = require("./email_text")
 const connect_to_google = require("./google")
-const readFileAsync = util.promisify(fs.readFile)
-const writeFileAsync = util.promisify(fs.writeFile)
 
 /* Sets which account the emails will be sent from */
-let transporter = nodemailer.createTransport({
-    service: "gmail",
+const email_transport_options = {
+    service: process.env.EMAIL_SERVICE,
     auth: {
-        user: "rboesp@gmail.com",
-        pass: process.env.GMAIL_PASSWORD,
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASSWORD,
     },
     tls: {
         rejectUnauthorized: false,
     },
-})
+}
+const email_service = nodemailer.createTransport(email_transport_options)
+
+
+/* *DONE SETUP */
+
+
+/*FUNCTIONS*/
+
 
 /**
- * Writes to actual email blast to all the addresses passed in
+ * writes actual email from mail options passed in 
+ * @param {Object} mail_options 
+ * @param {Function} resolve 
+ * @param {Function} reject 
+ */
+const sendEmails = (mail_options, resolve, reject) => {
+    /**This sends the actual emails to the recipients in mailOptions */
+    email_service.sendMail(mail_options, (error, info) => {
+        if (error) reject(error)
+        else {
+            /**This logs out info for a successfull email blast */
+            console.log();
+            console.log(info);
+            resolve()
+        }
+    })
+}
+
+
+/**
+ *Sets up email blast to all the addresses passed in
  * @param {Object} emails
  * @return {void}
  */
-const writeEmailToAll = async (emails) => {
-    return new Promise((resolve, err) => {
-        e_arr = []
-        emails.map((email) => e_arr.push(email[0]))
-        console.log("**Sending emails to: **")
-        console.log(e_arr)
-        let mailOptions = {
-            from: "rboesp@gmail.com",
-            to: ``,
-            cc: "centraltutorsupport@bootcampspot.com",
-            bcc: `${e_arr}`,
-            subject: `Coding Boot Camp - Tutor Available`,
-            html: create_email.email_body("https://calendly.com/robertboespflug/tutorial-session"),
-        }
+const setupEmailOptions = async (emails) => {
+    /*
+    this sets up the email structrure 
+    with onc cc and an array of bcc recepients
+    */
+    const options = {
+        from: "rboesp@gmail.com",
+        to: ``,
+        cc: "centraltutorsupport@bootcampspot.com ",
+        bcc: `${emails}`,
+        subject: `Coding Boot Camp - Tutor Available`,
+        html: create_email.email_body("https://calendly.com/robertboespflug/tutorial-session"),
+    }
 
-        transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                err(error)
-            } else {
-                console.log("Email sent: " + info.response)
-                resolve()
-            }
-        })
-    })
+    return new Promise(
+        (resolve, reject) => sendEmails(options, resolve, reject)
+    )
 }
+
+
+/**
+ * Exctracts studetns emails from google sheets api call
+ * @param {String} error 
+ * @param {Object} response 
+ */
+const handleSheetsResponse = (error, response) => {
+    if (error) return console.log("The API returned an error: " + error)
+
+    //exctract student emails from response
+    const rows = response.data.values
+
+   /**This logs out the list of student emails returned from google sheet */
+    console.log(rows)
+
+    /**This sends the email blast to student emails in google sheet */
+    const recipients = rows.map((row) => row[0])
+    setupEmailOptions(recipients)
+}
+
 
 /**
  * Get student emails from spreadsheet so we can can write email to them
  * @see https://docs.google.com/spreadsheets/d/1hiRdYWOxF1-yYHqIcSr8ebGiCLXTQXvgNbqqFtSLPqY/edit
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
-function start(auth) {
+const getGoogleSheet = (auth) => {
     const sheets = google.sheets({ version: "v4", auth })
-    sheets.spreadsheets.values.get(
-        {
-            spreadsheetId: "1rr4_HHt3B8Ci4lIP-EK6pn2DD2XIFh4G_zzclBzUlpA",
-            range: "Student Roster!D3:D200",
-        },
-        (err, res) => {
-            if (err) return console.log("The API returned an error: " + err)
-            const rows = res.data.values
-
-            /*TO TEST UNCOMMENT LOG BELOW AND COMMENT OUT FUNCTION CALL */
-            // console.log(rows)
-            writeEmailToAll(rows)
-        }
-    )
+    const sheetInfo = {
+        spreadsheetId: process.env.SPREADSHEETID,
+        range: process.env.SPREADSHEETRANGE,
+    }
+    sheets.spreadsheets.values.get(sheetInfo, handleSheetsResponse)
 }
 
-/*ENTRY POINT */
-const main = () => {
-    // Load client secrets from a local file.
-    fs.readFile("credentials.json", (err, content) => {
-        if (err) return console.log("Error loading client secret file:", err)
-        // Authorize a client with credentials
-        // Then call the Google Sheets API.
-        connect_to_google.authorize(JSON.parse(content), start) //USER DEFINED CALLBACK FUNCTION PASSED IN
-    })
+
+/*ENTRY POINT / START OF PROGRAM*/
+const main = async () => {
+    // Load client secrets from a local file
+    const credentials = await readFileAsync("credentials.json")
+
+    //connect to google sheets api
+    //USER DEFINED CALLBACK FUNCTION PASSED IN
+    connect_to_google.authorize(JSON.parse(credentials), getGoogleSheet)  
 }
 main()

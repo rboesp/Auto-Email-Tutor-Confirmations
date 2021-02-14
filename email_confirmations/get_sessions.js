c3ccd121c9df61c89c6d182806f762c3f3549b36
Robert Boespflug <boespflr@oregonstate.edu>
2021-02-13 22:46:37 -0800

    fixed credentials not found error

--- email_confirmations/get_sessions.js
+++ email_confirmations/get_sessions.js
@@ -41,7 +41,7 @@ const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 const TOKEN_PATH = "token.json"
 
 // Load client secrets from a local file.
-fs.readFile("credentials.json", (err, content) => {
+fs.readFile("email_confirmations/credentials.json", (err, content) => {
     if (err) return console.log("Error loading client secret file:", err)
     // Authorize a client with credentials, then call the Google Calendar API.
     authorize(JSON.parse(content), listEvents)

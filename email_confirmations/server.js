fab69332d9f68eff67117283ad2af6d0dc5c792a
Robert Boespflug <boespflr@oregonstate.edu>
2021-02-21 21:27:48 -0800

    need to make three different heroku projects, three api calls and put env variables, taoken, credentials in heroku env

---
+++ email_confirmations/server.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 3000
+
+const childProcess = require("child_process")
+const fs = require("fs")
+
+function run(scriptPath, callback) {
+    // keep track of whether callback has been invoked to prevent multiple invocations
+    var invoked = false
+
+    var process = childProcess.fork(scriptPath)
+
+    // listen for errors as they may prevent the exit event from firing
+    process.on("error", function (err) {
+        if (invoked) return
+        invoked = true
+        callback(err)
+    })
+
+    // execute the callback once the process has finished running
+    process.on("exit", function (code) {
+        if (invoked) return
+        invoked = true
+        var err = code === 0 ? null : new Error("exit code " + code)
+        callback(err)
+    })
+}
+
+function runScript(script) {
+    return new Promise((resolve, reject) => {
+        // Now we can run a script and invoke a callback when complete, e.g.
+        run(`./${script}`, function (err) {
+            if (err) reject(err)
+            resolve(`FINISHED RUNNING ${script} \n`)
+        })
+    })
+}
+
+async function getEmailConfirmations() {
+    console.log("Sending email confirmations!!")
+    let msg = await runScript("get_sessions.js")
+    console.log(msg)
+    msg = await runScript("email_controller.js")
+    console.log(msg)
+    msg = await runScript("send_email.js")
+    console.log(msg)
+    console.log("Done sending email confirmations!!")
+    return 'Sent email confirmations!'
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/confs', (req, res) => {
+    const ret = getEmailConfirmations()
+    res.send(ret)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+
+
+
